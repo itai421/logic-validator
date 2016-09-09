0a3e78a5ac48e50515e138edfa518390a6a1f75a
itai vaks <itai421@gmail.com>
2016-09-09 16:29:13 +0300

    adding generic conditions

---
+++ conditions/NotEquals.js
@@ -0,0 +1,20 @@
+"use strict";
+
+//TODO: use the export and require directly
+let Condition = require('./../lib/Condition');
+
+//TODO: isn't that to generic?
+class NotEquals extends Condition {
+
+    constructor(value1, value2){
+        super();
+        this.value1 = value1;
+        this.value2 = value2;
+    }
+
+    validate(){
+        return this.value1 != this.value2;
+    }
+}
+
+module.exports = NotEquals;
