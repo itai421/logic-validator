5dc96fa03bf5b77bab4dca3feea2f9ea275ba4d8
itai vaks <itai421@gmail.com>
2016-10-03 17:43:28 +0300

    first working version adding test for GreaterThan

---
+++ test/GreaterThan.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let should = require('should/as-function');
+let GreaterThan = require('../conditions/GreaterThan');
+
+describe('GreaterThan', function () {
+    describe('#validate()', function () {
+        context('when the values are equal', function () {
+            it('should return return false', function () {
+                let condition = new GreaterThan(6,6);
+                should(condition.validate()).not.be.true();
+            });
+        });
+        context('when the first value is greater than the second one', function () {
+            it('should return return true', function () {
+                let condition = new GreaterThan(6,5);
+                should(condition.validate()).be.true();
+            });
+        });
+        context('when the first value is less than the second one', function () {
+            it('should return return fasle', function () {
+                let condition = new GreaterThan(5,6);
+                should(condition.validate()).not.be.true();
+            });
+        });
+    });
+});
