27cb407e2cf8ef96b715dc3755c16f20cee35bb6
itai vaks <itai421@gmail.com>
2016-10-03 14:56:24 +0300

    adding first pending test

---
+++ test/test.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let should = require('should/as-function');
+describe('Condition', function(){
+    describe('#validate()', function(){
+        it('should return return true or false');
+    });
+});
