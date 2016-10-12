da9eeeb7b437cccc60f1b97be56abac40fe6b95d
itai vaks <itai421@gmail.com>
2016-10-12 17:21:40 +0300

    removing unused code

--- examples/NumbersValidator.js
+++ examples/NumbersValidator.js
@@ -1,24 +1,11 @@
 "use strict";
 
-/*
-*
-* you have three numbers
-* you need to make sure that:
-* #1 < #2
-* #2 != #3
-* #1 == #3
-*
-*
-* */
-
 let Validator = require('logic-validator').Validator;
 let AndCondition = require('logic-validator').AndCondition;
 let LessThan = require('logic-validator/conditions').LessThan;
 let NotEquals = require('logic-validator/conditions').NotEquals;
 let Equals = require('logic-validator/conditions').Equals;
 
-//TODO: add config;
-// let maxInvitationsNumber = config.get('maxInvitationsNumber');
 let maxInvitationsNumber = 80;
 
 class NumbersValidator extends Validator{
